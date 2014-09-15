fa63239750103be21191ddda20aecaf57a68b1b8
tankorsmash <tankorsmash@gmail.com>
2014-09-15 00:52:12 -0400

    handle vids with timestamps already existing

--- scripts/content_ytt.js
+++ scripts/content_ytt.js
@@ -11,7 +11,7 @@ get_selection = function() {
 $(function() {
 
     if ($("#player").length) {
-        $("#player").prepend("<a id='copylink'>COPY SELECTION</a>")
+        $("#player").prepend("<a id='copylink'>Copy timestamp</a>")
         $("#copylink").click(send_selection);
     }
     else {
@@ -20,7 +20,15 @@ $(function() {
 });
 
 function get_player_time()  {
-    return $(".html5-progress-bar.red").attr("aria-valuenow");
+    var time_sec =  $(".html5-progress-bar.red").attr("aria-valuenow");
+    var url = document.location.href;
+    var parsed = purl(url);
+    if (parsed.fparam("t") !== undefined)
+    {
+        url = url.replace("#t="+parsed.fparam("t"), "");
+    };
+    var final_url = url + "#t=" + time_sec;
+    return final_url;
 }
 
 function send_selection(e)  {
